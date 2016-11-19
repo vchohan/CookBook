4032194592699a44be4f8f41c3b91b48377812cf
Vineeth Chohan <vineeth.chohan@gmail.com>
2016-11-18 20:01:23 -0500

    Cook Book - 2nd Commit

--- app/src/main/java/com/vchohan/cookbook/AddNewRecipeFragment.java
+++ app/src/main/java/com/vchohan/cookbook/AddNewRecipeFragment.java
@@ -1,14 +1,13 @@
 package com.vchohan.cookbook;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-/**
- * Created by vchohan on 11/18/16.
- */
+import android.widget.Button;
+import android.widget.Toast;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -16,6 +15,8 @@ import android.view.ViewGroup;
 public class AddNewRecipeFragment extends Fragment {
 
     public static final String TAG = AddNewRecipeFragment.class.getSimpleName();
+
+    private static Button recipeSaveButton;
 
     /**
      * The fragment argument representing the section number for this
@@ -39,6 +40,22 @@ public class AddNewRecipeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
         Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.add_new_recipe_fragment, container, false);
+
+        initializeView(rootView);
+
         return rootView;
+    }
+
+    private void initializeView(View rootView) {
+        recipeSaveButton = (Button) rootView.findViewById(R.id.save_button);
+        recipeSaveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String recipeSaved = getString(R.string.recipe_saved);
+                Toast.makeText(getContext(), recipeSaved, Toast.LENGTH_SHORT).show();
+
+                Intent 
+            }
+        });
     }
 }
