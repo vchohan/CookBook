6c37e819a9316aa435bc0ebee82a5d0f40bea5b0
Vineeth Chohan <vineeth.chohan@gmail.com>
2016-12-17 19:37:42 -0500

    Cook Book - 25th Commit *More UI Refactoring

--- cookbook/src/main/java/com/vchohan/cookbook/SplashActivity.java
+++ cookbook/src/main/java/com/vchohan/cookbook/SplashActivity.java
@@ -1,82 +1,50 @@
 package com.vchohan.cookbook;
 
-import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ProgressBar;
 
 public class SplashActivity extends AppCompatActivity {
 
-    private ProgressDialog mProgressDialog;
+    private ProgressBar mProgressView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_activity);
-        showProgressDialog();
 
-// METHOD 1
-        /****** Create Thread that will sleep for 5 seconds *************/
-        Thread background = new Thread() {
+        mProgressView = (ProgressBar) findViewById(R.id.splash_progress_bar);
+
+        // Start lengthy operation in a background thread
+        new Thread(new Runnable() {
             public void run() {
-                try {
-                    // Thread will sleep for 5 seconds
-                    sleep(3 * 1000);
-
-                    // After 5 seconds redirect to another intent
-                    Intent mainIntent = new Intent(getBaseContext(), MainActivity.class);
-                    mainIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                    startActivity(mainIntent);
-
-                    //Remove activity
-                    hideProgressDialog();
-                    finish();
-
-                } catch (Exception e) {
-
-                }
-            }
-        };
-
-        // start thread
-        background.start();
-
-//METHOD 2
-        /*
-        new Handler().postDelayed(new Runnable() {
-            // Using handler with postDelayed called runnable run method
-            @Override
-            public void run() {
-                    Intent loginIntent = new Intent(getBaseContext(), LoginActivity.class);
-                    loginIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                    startActivity(loginIntent);
-
-                // close this activity
-                hideProgressDialog();
+                setProgressBar();
+                startApp();
                 finish();
             }
-        }, 5*1000); // wait for 5 seconds
-        */
+        }).start();
+    }
+
+    private void setProgressBar() {
+        for (int progress = 0; progress < 100; progress += 10) {
+            try {
+                Thread.sleep(1000);
+                mProgressView.setProgress(progress);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void startApp() {
+        Intent mainIntent = new Intent(getBaseContext(), MainActivity.class);
+        mainIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(mainIntent);
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-
-    }
-
-    private void showProgressDialog() {
-        if (mProgressDialog == null) {
-            mProgressDialog = new ProgressDialog(this);
-            mProgressDialog.setMessage("Loading...");
-            mProgressDialog.setIndeterminate(true);
-        }
-        mProgressDialog.show();
-    }
-
-    private void hideProgressDialog() {
-        if (mProgressDialog != null && mProgressDialog.isShowing()) {
-            mProgressDialog.dismiss();
-        }
     }
 }
