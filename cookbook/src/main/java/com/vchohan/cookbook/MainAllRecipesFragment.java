acd2071ea1b47e50427ddca4bd5f41864a735e0d
Vineeth Chohan <vineeth.chohan@gmail.com>
2017-01-03 22:52:35 -0500

    Cook Book - 44th Commit *Adding Favorite RecyclerView

--- cookbook/src/main/java/com/vchohan/cookbook/MainAllRecipesFragment.java
+++ cookbook/src/main/java/com/vchohan/cookbook/MainAllRecipesFragment.java
@@ -14,6 +14,7 @@ import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -25,6 +26,8 @@ import com.firebase.ui.database.FirebaseRecyclerAdapter;
  */
 
 public class MainAllRecipesFragment extends Fragment {
+
+    public static final String TAG = MainAllRecipesFragment.class.getSimpleName();
 
     private RecyclerView mRecipeRecyclerView;
 
@@ -162,7 +165,7 @@ public class MainAllRecipesFragment extends Fragment {
 
                             @Override
                             public void onCancelled(DatabaseError databaseError) {
-
+                                Log.w(TAG, "getUser:onCancelled", databaseError.toException());
                             }
                         });
                     }
--- cookbook/src/main/java/com/vchohan/cookbook/MainFavoriteRecipesFragment.java
+++ cookbook/src/main/java/com/vchohan/cookbook/MainFavoriteRecipesFragment.java
@@ -1,7 +1,14 @@
 package com.vchohan.cookbook;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.Query;
+
+import android.app.ProgressDialog;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,6 +18,30 @@ import android.view.ViewGroup;
  */
 
 public class MainFavoriteRecipesFragment extends Fragment {
+
+    public static final String TAG = MainFavoriteRecipesFragment.class.getSimpleName();
+
+    private RecyclerView mRecipeRecyclerView;
+
+    private LinearLayoutManager mLinearLayoutManager;
+
+    private FirebaseAuth mAuth = null;
+
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    private DatabaseReference mDatabaseRecipes;
+
+    private DatabaseReference mDatabaseUsers;
+
+    private DatabaseReference mDatabaseCurrentUser;
+
+    private Query mQueryCurrentUser;
+
+    private DatabaseReference mDatabaseLikes;
+
+    private boolean mProcessLike = false;
+
+    private ProgressDialog mProgressDialog;
 
     public MainFavoriteRecipesFragment() {
         // Required empty public constructor
@@ -25,7 +56,15 @@ public class MainFavoriteRecipesFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
         Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.main_recipe_recycle_view, container, false);
+        View rootView = inflater.inflate(R.layout.main_recipe_recycle_view, container, false);
+
+//        initializeFirebaseAuth();
+//        initializeFirebaseDatabase();
+//        initializeRecyclerView(rootView);
+
+        return rootView;
     }
+
+
 
 }
